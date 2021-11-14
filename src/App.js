9f6ba1b292d2b6b8f90edfd2ce181b1ab43fe025
Johanna Farace <Johanna.farace@gmail.com>
2021-11-14 13:05:31 +0100

    covid tracker api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch("https://api.covidtracking.com/v1/us/daily.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className='app'>
+        <h1>Covid cases tracker</h1>
+      <ul className='container'>
+        {console.log(items)}
+        
+        {items.map((item, i) => (
+          <li className='row' key={item.hash}>
+            <div className='date'>Date: {item.dateChecked}</div>
+            <div className='infections'>Number of infections: {item.positive}</div> 
+            <div className='deaths'>Deaths: {item.death}</div>
+          </li>
+        ))}
+      </ul>
+      </div>
+      
+      
+    );
+  }
 }
 
 export default App;
+
